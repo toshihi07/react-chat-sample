7442ed991a6302f62da419c9bf4ff2703b3c16c7
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-02 18:34:33 +0900

    setname-by-clicking-button

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -45,6 +45,14 @@ const useStyles = makeStyles((theme) => ({
 //setNameをどこで発火されるのかを考えてみる。「はじめる」を押した時に発火させる。
 export default function SignIn( { setName } ) {
   const classes = useStyles();
+  const [disabled,setDisabled] = useState(true);
+  const [string,setString] = useState('');
+  console.log({ disabled,string })
+
+useEffect(() =>{
+  const disabled = string === ""
+  setDisabled(disabled);
+},[string]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -64,6 +72,7 @@ export default function SignIn( { setName } ) {
             name="name"
             // autoComplete="email"
             autoFocus
+            onChange={(e) => setString(e.target.value)}
           />
           <Button
             type="submit"
@@ -71,6 +80,7 @@ export default function SignIn( { setName } ) {
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled={ disabled }
           >
             はじめる
           </Button>
