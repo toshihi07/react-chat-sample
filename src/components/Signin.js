a519403aaac9211fb948b798099106b6c2e5fbb9
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-03 14:29:45 +0900

    save-namme-by-enter-key

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -73,6 +73,13 @@ useEffect(() =>{
             // autoComplete="email"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown= {(e) => {
+              if(e.key === "Enter") {
+                setName(e.target.value)
+                e.preventDefault();
+              }
+         }
+          }
           />
           <Button
             type="button"
