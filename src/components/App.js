e06bc94a5cec452bd2958793c50770814c302e21
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-03 15:03:40 +0900

    set-name-in-japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import Signin from "./Signin";
+import Main from './main'
 
-// export default () => {}
+// export default () => {}。functiojnを省略している
 export default () => {
   //app.jsで定義。
   const [name,setName] = useState('');
   //javascriptの変数は{}で囲む
   console.log( {name} )
-  return (
+  //componentの切り替え
+  if (name === ""){
+    return (
       <Signin setName={ setName }/>
   );
+  } else {
+    return (
+      <Main name={ name }/>
+    )
+  }
 };
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -47,7 +47,8 @@ export default function SignIn( { setName } ) {
   const classes = useStyles();
   const [disabled,setDisabled] = useState(true);
   const [string,setString] = useState('');
-  console.log({ disabled,string })
+  const [isComposed,setIsComposed] = useState(false);
+  console.log({ disabled,string, isComposed})
 
 useEffect(() =>{
   const disabled = string === ""
@@ -74,11 +75,18 @@ useEffect(() =>{
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown= {(e) => {
+              if (isComposed) return;
               if(e.key === "Enter") {
                 setName(e.target.value)
                 e.preventDefault();
               }
          }
+          }
+          onCompositionStart={
+            () => setIsComposed(true)
+          }
+          onCompositionEnd = {
+            () =>setIsComposed(false)
           }
           />
           <Button
