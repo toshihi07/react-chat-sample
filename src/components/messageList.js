e97b68ec83c876bccd0bf2b01017e7ac2ed887e4
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-04 16:33:02 +0900

    grid_layout

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
--- src/components/messageListInputField.js
+++ src/components/messageListInputField.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+//Gridはcoreから呼び出す
+import { Grid, Avatar } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
-  }
-})
+    margin: "26px"
+  },
+});
 
-
-const MessageListInputField = () =>{
+const MessageListInputField = () => {
   const classes = useStyles();
-return <div className={classes.root}>messageListInputField</div>
-}
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>
+          <input type='text' name='txt' placeholder='入力欄' require />
+        </Grid>
+        <Grid xs={1}>
+          ボタン
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default MessageListInputField;
