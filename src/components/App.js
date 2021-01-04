0ce3743ad51b0db1dd890999e455a264f07f428c
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-04 11:16:55 +0900

    reate-config-json

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import Signin from "./Signin";
 import Main from './main'
+import config from '../config.json'
+console.log(config)
 
 // export default () => {}。functiojnを省略している
 export default () => {
   //app.jsで定義。
   const [name,setName] = useState('');
+  const signInEnabled = config.signInEnabled
+  console.log(signInEnabled)
   //javascriptの変数は{}で囲む
   console.log( {name} )
   //componentの切り替え
-  if (name === ""){
+  if (name === "" && signInEnabled){
     return (
       <Signin setName={ setName }/>
   );
