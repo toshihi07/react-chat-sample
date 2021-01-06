54be9300de3d99abb0e8d7485e83fc1116659052
Toshihiro Shinmachi <toshihi0717@gmail.com>
2021-01-06 16:40:07 +0900

    display-messages

---
+++ src/components/MessageItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+  Avatar,
+} from "@material-ui/core";
+
+import {gravatarPath} from '../gravatar'
+
+const useStyles = makeStyles((theme) => ({
+  inline: {
+    display: 'inline',
+  },
+}));
+
+
+const MessageItem = ({ name, key, text }) => {
+  const classes = useStyles
+  const avatarPath = gravatarPath(name)
+
+  return (
+    <ListItem alignItems="flex-start" divider={true}>
+    <ListItemAvatar>
+      <Avatar src={avatarPath} />
+    </ListItemAvatar>
+    <ListItemText
+      primary={name}
+      secondary={
+          <Typography
+            component="span"
+            variant="body2"
+            className={classes.inline}
+            color="textPrimary"
+          >
+            {text}
+          </Typography>
+      }
+    />
+  </ListItem>
+  )
+};
+
+export default MessageItem;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,44 +1,49 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { List, ListItem } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
 import { messagesRef } from "../firebase";
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    overflow:"auto",
+    width: '100%',
   },
 });
 
 const MessageList = () => {
-  const [messages,setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
   //useEffect内の関数はレンダリング時の一度しか行われない。第２引数にから配列を渡すことで、一度しか行わないことを明示的に伝えている。
-  useEffect(() =>  messagesRef
-  .orderByKey()
-  .limitToLast(3)
-  .on(
-    "value",
-    function (snapshot) {
-      const messages = snapshot.val();
-      console.log(messages)
-      if(messages == null){
-        return
-      }
-      const entries = Object.entries(messages);
-      const newMessages = entries.map((entry) => {
-        // エントリー0はキー
-        // const key = entry[0]
-        // const nameAndText = entry[1];
-        const [key,nameAndText] = entry
-        return {
-          //{name: "ハムさん", text: "aaaa"}の形で取り出せる
-          key,...nameAndText
-        }
-      });
-      console.log(newMessages)
-      setMessages(newMessages)
-      console.log("useEffect")
-    },
-  ),[]);
+  useEffect(
+    () =>
+      messagesRef
+        .orderByKey()
+        .limitToLast(20)
+        .on("value", function (snapshot) {
+          const messages = snapshot.val();
+          console.log(messages);
+          if (messages == null) {
+            return;
+          }
+          const entries = Object.entries(messages);
+          const newMessages = entries.map((entry) => {
+            // エントリー0はキー
+            // const key = entry[0]
+            // const nameAndText = entry[1];
+            const [key, nameAndText] = entry;
+            return {
+              //{name: "ハムさん", text: "aaaa"}の形で取り出せる
+              key,
+              ...nameAndText,
+            };
+          });
+          console.log(newMessages);
+          setMessages(newMessages);
+          console.log("useEffect");
+        }),
+    []
+  );
   //useStyllesを実行してスタイルを取得。
   const classes = useStyles();
   // Attach an asynchronous callback to read the data at our posts reference
@@ -48,9 +53,15 @@ const MessageList = () => {
   // 他人にもわかりやすいようにコードを書くようにすること
   // コンポーネントがマウントされる際に一度だけ処理を実行するuseEffectを使わないと、無限ループに陥る。
 
-
-  return <div className={classes.root}>messageList</div>;
+  return (
+    <List className={classes.root}>
+      {messages.map(({ key, name, text }) => {
+        return <MessageItem key={key} name={name} text={text}>item</MessageItem>;
+      })}
+    </List>
+  );
 };
 
 export default MessageList;
+
 
